b763f3370969583eb11187fa4f8e473220ce3ad4
Justin Duch <aphelion030@gmail.com>
2018-07-27 15:55:46 +1000

    finish front end sort of

--- react/src/LoadingScreen.jsx
+++ react/src/LoadingScreen.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 export default class LoadingScreen extends React.Component {
     render() {
         return (
-            <div>
+            <div className="main fade-in">
                 <h1>Loading</h1>
+                <div class="sk-folding-cube">
+                    <div class="sk-cube1 sk-cube"></div>
+                    <div class="sk-cube2 sk-cube"></div>
+                    <div class="sk-cube4 sk-cube"></div>
+                    <div class="sk-cube3 sk-cube"></div>
+                </div>
+                <p>Please wait while the website is being crawled.</p>
             </div>
         )
     }
--- react/src/ResultsScreen.jsx
+++ react/src/ResultsScreen.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 
 import LoadingScreen from './LoadingScreen';
-import ErrorScreen from './ErrorScreen';
 
 export default class ResultsScreen extends React.Component {
     constructor(props) {
@@ -23,10 +22,12 @@ export default class ResultsScreen extends React.Component {
                 q: this.props.search
             }
         }).then(function(response) {
+            const data = JSON.parse(response.data);
+
             self.setState({
                 loading: false,
-                results: response.data,
-                error: response.data.error,
+                results: data,
+                error: data.error
             });
         });
     }
@@ -37,19 +38,38 @@ export default class ResultsScreen extends React.Component {
         if (loading) return <LoadingScreen />
         if (error)   return <ErrorScreen error={error} />
 
-        return <ResultsField results={results} />
+        return <ResultsField results={results} website={this.props.website} item={this.props.search} />
     }
 }
 
 function ResultsField(props) {
     return (
-        <div>Hello</div>
+        <div className="results-field">
+            <h1>Search results for <i>{props.website}</i></h1>
+            <p><small>Search item: <b><i>{props.item}</i></b></small></p>
+            {props.results.map((result, i) => <ResultBox result={result} key={i} />)}
+        </div>
     )
 }
 
 function ResultBox(props) {
     return (
-        <div>
+        <div className="result-box">
+            <a href={props.result.uri}>
+                <p className="result-title">{props.result.content.title}</p>
+                <p className="result-link"><small>{props.result.uri}</small></p>
+            </a>
+            <p>{props.result.content.blurb}</p>
+        </div>
+    )
+}
+
+function ErrorScreen(props) {
+    return (
+        <div className="main">
+            <h1>Error {props.error.status}</h1>
+            <h3>"{props.error.message}"</h3>
+            <p>Click <a href="/">here</a> to go back</p>
         </div>
     )
 }
